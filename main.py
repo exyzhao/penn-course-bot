ca002e9716ad37ce4fe11417984b40055a7bb52b
Brandon Wang <brandonbigbrother@gmail.com>
2020-12-09 00:37:34 -0600

    add twilio support

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 import requests
 import json
 from secrets import Secrets
+from twilio.rest import Client
 
 
 def get_course_status():
@@ -20,6 +21,7 @@ def get_course_status():
             notif: str = f"{current_time}: {course} is open!"
             print(notif)
             print(post_groupme_message(notif))
+            # send_twilio_sms(target_num, notif) # disable for now?
         else:
             print(f"{current_time}: {course} is closed!")
         return 0
@@ -44,9 +46,27 @@ def post_groupme_message(msg: str):
         return None, r.text
 
 
+def send_twilio_sms(phone_num: str, msg: str):
+    # Your Account Sid and Auth Token from twilio.com/console
+    # and set the environment variables. See http://twil.io/secure
+    account_sid = Secrets.TWILIO_ACCOUNT_SID
+    auth_token = Secrets.TWILIO_AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body=msg,
+        from_='4154032721',  # hardcoded for now
+        to=phone_num
+    )
+
+    print(message.sid)
+
+
 if __name__ == '__main__':
     course = "CIS 121001"
     group_id = "64423915"
+    target_num = "4699316958" # sms destination
     interval = 10.0  # Request interval, in seconds (should have 6000/hr)
     GROUPME_URL = f"https://api.groupme.com/v3/groups/{group_id}/messages?token={Secrets.GM_TOKEN}"
     get_course_status()
