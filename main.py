dd952ba6a4bd355f5286d288c52c44de89e5536f
Brandon Wang <brandonbigbrother@gmail.com>
2020-12-09 21:55:26 -0600

    rewrite to enable multiple courses hit the all courses endpoint (dont exceed 250 req/hr?)

--- main.py
+++ main.py
@@ -9,34 +9,49 @@ import json
 from secrets import Secrets
 from twilio.rest import Client
 
-
+# https://esb.isc-seo.upenn.edu/8091/documentation/#coursestatusservice
 def get_course_status():
     threading.Timer(interval, get_course_status).start()
 
     try:
-        output = get_all_course_status("2021A", course)
+        output = get_all_course_status("2021A", "all")  # hit all endpoints
+        print("courses loaded")
     except RuntimeError:
         raise SystemExit("Course Fetch had an error")
 
     current_time = datetime.now().strftime("%H:%M:%S")
-    if output[0]['status'] == 'O':
-        notif: str = f"{current_time}: {course} is open!"
-        print(notif)
-        print(post_groupme_message(notif))
-        # send_twilio_sms(target_num, notif) # disable for now?
-    else:
-        print(f"{current_time}: {course} is closed!")
+    for entry in output:
+        entry_name: str = entry['course_section']
+        # Twilio SMS messages
+        if entry_name in sms_alerts.keys():
+            if entry['status'] == 'O':  # if course is open
+                notif: str = f"{current_time}: {entry_name} is open!"
+                print(notif)
+                for p in sms_alerts[entry_name]:  # send alert to all phone nums
+                    print(send_twilio_sms(p, notif))
+            else:
+                print(f"{current_time}: {entry_name} is closed!")
+        # GroupMe messages
+        if entry_name in groupme_alerts.keys():
+            if entry['status'] == 'O':
+                notif: str = f"{current_time}: {entry_name} is open!"
+                print(notif)
+                for p in groupme_alerts[entry_name]:
+                    print(post_groupme_message(p, notif))
+            else:
+                print(f"{current_time}: {entry_name} is closed!")
     return 0
 
 
-def post_groupme_message(msg: str):
+def post_groupme_message(group_id: str, msg: str):
+    groupme_url = f"https://api.groupme.com/v3/groups/{group_id}/messages?token={Secrets.GM_TOKEN}"
     data = {
         "message": {
             "source_guid": str(round(time.time() * 1000)),
             "text": msg
         }
     }
-    r = requests.post(GROUPME_URL,
+    r = requests.post(groupme_url,
                       data=json.dumps(data),
                       headers={'Content-Type': 'application/json'})
 
@@ -48,27 +63,24 @@ def post_groupme_message(msg: str):
 
 def send_twilio_sms(phone_num: str, msg: str):
     # Your Account Sid and Auth Token from twilio.com/console
-    # and set the environment variables. See http://twil.io/secure
     account_sid = Secrets.TWILIO_ACCOUNT_SID
     auth_token = Secrets.TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token)
 
-    message = client.messages \
-        .create(
+    message = client.messages.create(
         body=msg,
         from_='4154032721',  # hardcoded for now
         to=phone_num
     )
 
-    print(message.sid)
+    return message.sid
 
 
 if __name__ == '__main__':
-    # write a dict from courses to list of #s?, then hit the all courses end point
-    alerts = {"CIS 121001": ["4699316958"]}
-    course = "CIS 121001"
-    group_id = "64423915"
-    target_num = "4699316958"  # sms destination
-    interval = 10.0  # Request interval, in seconds (should have 6000/hr)
-    GROUPME_URL = f"https://api.groupme.com/v3/groups/{group_id}/messages?token={Secrets.GM_TOKEN}"
+    # Maps course id to phone num
+    sms_alerts = {"CIS 120001": ["4699316958"]}
+    # Maps course id to GroupMe group num
+    groupme_alerts = {"CIS 121001": ["64423915"],
+                      "CIS 262001": ["64440931"]}
+    interval = 15.0  # Request interval, in seconds (should have 6000/hr cap)
     get_course_status()
